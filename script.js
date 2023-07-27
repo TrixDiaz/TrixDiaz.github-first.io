559426cb3a057ef3060e86524d5fb6dcb5a5dc8a
XirTech <john.darlucio022@gmail.com>
2023-07-27 17:52:33 +0800

    change style

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
 console.log("Welcome to Console");
-//Toggle Navigation Icon 
-const icon = document.getElementById('navIcon');
-const links = document.getElementById('links');
-const bootStrapIcon = document.getElementById('icons');
-
-  icon.addEventListener('click', () => {
- const toggle =  links.classList.toggle('links-toggle'); 
-  bootStrapIcon.classList = toggle
-      ? 'bi bi-x-lg' 
-      : 'bi bi-list-nested'
-    });
