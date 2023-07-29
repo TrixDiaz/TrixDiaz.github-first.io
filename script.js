87990ec068b22d2de2dfc7f6ce89f1e1f3d3cee9
XirTech <john.darlucio022@gmail.com>
2023-07-29 21:54:51 +0800

    add keyframes

--- script.js
+++ script.js
@@ -4,57 +4,49 @@ console.log("Welcome to Console");
 const mode = () => {
   // Toggle the 'light-mode' class on the body element
   const body = document.body;
-  const changeIcon = document.getElementById('icon');
-  const menuChangeIcon = document.getElementById('menu-icon');
-  body.classList.toggle('light-mode');
+  const changeIcon = document.getElementById("icon");
+  const menuChangeIcon = document.getElementById("menu-icon");
+  body.classList.toggle("light-mode");
 
-   const isLightMode = body.classList.contains('light-mode');
-   changeIcon.classList = isLightMode
-      ? 'bi bi-moon-stars-fill' 
-      : 'bi bi-brightness-high' 
-   menuChangeIcon.classList = isLightMode
-      ? 'bi bi-moon-stars-fill' 
-      : 'bi bi-brightness-high' 
-      changeIcon.style.transition = "1s ease";
+  const isLightMode = body.classList.contains("light-mode");
+  changeIcon.classList = isLightMode
+    ? "bi bi-moon-stars-fill"
+    : "bi bi-brightness-high";
+  menuChangeIcon.classList = isLightMode
+    ? "bi bi-moon-stars-fill"
+    : "bi bi-brightness-high";
+  changeIcon.style.transition = "1s ease";
 };
 
 // Define the arrow function to toggle open navigation
 const toggleNav = () => {
-  const toggleNav = document.getElementById('toggle-nav');
+  const toggleNav = document.getElementById("toggle-nav");
 
-  const isOpen = toggleNav.classList.toggle('open')
-  toggleMenu.classList = isOpen
-    ? 'bi bi-x-lg'
-    : 'bi bi-list-nested'
-    toggleMenu.style.transition = "2s ease";
-   
-
+  const isOpen = toggleNav.classList.toggle("open");
+  toggleMenu.classList = isOpen ? "bi bi-x-lg" : "bi bi-list-nested";
+  toggleMenu.style.transition = "2s ease";
 };
 
 // Add a click event listener to the icon element
-const icon = document.getElementById('icon');
-icon.addEventListener('click', mode);
+const icon = document.getElementById("icon");
+icon.addEventListener("click", mode);
 
 // Add a click event listener to the menu icon element
-const menuIcon = document.getElementById('menu-icon');
-menuIcon.addEventListener('click', mode);
+const menuIcon = document.getElementById("menu-icon");
+menuIcon.addEventListener("click", mode);
 
 // Add a click event listener to the toggle menu element
-const toggleMenu = document.getElementById('toggle-menu');
-toggleMenu.addEventListener('click', toggleNav);
+const toggleMenu = document.getElementById("toggle-menu");
+toggleMenu.addEventListener("click", toggleNav);
 
 // Loop Typing Text
-const texts = [
-  "Developer",
-  "IT Specialist",
-  "UI/UX Designer",
-  "IT Technician"
-];
+const texts = ["Developer", "IT Specialist", "UI/UX Designer", "IT Technician"];
 
 const typingSpeed = 100; // Adjust this value to control the typing speed (in milliseconds)
 
 const typingElement = document.getElementById("typing-text");
 
+// Define function typeText
 function typeText(text, index) {
   if (index < text.length) {
     typingElement.textContent += text.charAt(index);
@@ -72,3 +64,5 @@ function typeText(text, index) {
 }
 
 typeText(texts[0], 0); // Start the typing animation with the first text
+
+
